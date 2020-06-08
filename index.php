901a33c5c42fea9702f737ffe4af4911744e91a5
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 09:12:27 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 2 exercice 2</title>
+</head>
+<body>
+    <p>
+        <?php
+            $isEasy = true;
+            if ($isEasy) {
+                    echo 'C\'est facile !';
+            }else {
+                echo 'C\'est difficile !!';
+            }
+        ?>
+    </p>
+</body>
+</html>
