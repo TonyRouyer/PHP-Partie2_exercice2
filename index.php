b9adb996a5613e878526efe91ee32df6e4a976e3
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-09 14:58:37 +0200

    correction

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>partie 2 exercice 2</title>
 </head>
 <body>
     <p>
         <?php
             $isEasy = true;
-             if ($isEasy) {
-                     echo 'C\'est facile !';
-             }else {
-                 echo 'C\'est difficile !!';
-             }
+            // if ($isEasy) {
+            //     echo 'C\'est facile !';
+            // }else {
+            //     echo 'C\'est difficile !!';
+            // }
+            echo $isEasy ? 'C\'est facile !' : 'C\'est difficile !!';
         ?>
     </p>
 </body>
